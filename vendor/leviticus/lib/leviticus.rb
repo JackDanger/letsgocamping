ab7117b9b95a44b71ff3f585111110c16b9467d0
Jack Danger Canty <gitcommit@6brand.com>
2011-05-28 15:52:50 -0700

    working toward a slimmed-down Leviticus page

--- vendor/leviticus/lib/leviticus.rb
+++ vendor/leviticus/lib/leviticus.rb
@@ -3,12 +3,17 @@ require File.expand_path File.join(File.dirname(__FILE__), 'page')
 
 module Leviticus
   class << self
+
     def output_to site
       @site = site
     end
 
     def views_directory views
       @views = views
+    end
+
+    def source source_string
+      @source = Leviticus::Source.new source_string
     end
 
     def site
@@ -28,6 +33,11 @@ module Leviticus
       @index
     end
 
+    def process! media = [:html]
+      raise "Source content not yet supplied" unless @source
+      @source.process media
+    end
+
     def write_all media = [:html]
       Dir.mkdir @site rescue ''
       media.each do |medium|
@@ -41,6 +51,16 @@ module Leviticus
         end
         puts "Wrote #{Leviticus.pages.sum {|_,pages| pages.size}} pages"
       end
+    end
+
+    # swiped from ActiveSupport
+    def underscore word
+      word = word.to_s.dup
+      word.gsub! /([A-Z]+)([A-Z][a-z])/,'\1_\2'
+      word.gsub! /([a-z\d])([A-Z])/,'\1_\2'
+      word.tr! "-", "_"
+      word.downcase!
+      word
     end
   end
 end
--- vendor/leviticus/lib/page.rb
+++ vendor/leviticus/lib/page.rb
@@ -1,23 +1,42 @@
 module Leviticus
-
   class Page
-    attr_accessor :path, :name, :links, :verses, :is_kindle
+    attr_accessor :path, :title, :medium
 
     def initialize *args
-      SilentVoices.pages << self
+      (Leviticus.pages[self.class] ||= []) << self
+    end
+
+    # Add a method to your page to make that value available in the view
+    # as a helper method:
+    #
+    #  class MySpecialPage < Leviticus::Page
+    #    def page_number
+    #      data[:current_page] # assuming you've implemented 'data' elsewhere
+    #    end
+    #  end
+    #
+    # Add any simple values you want to the page (like page number or author)
+    # just by calling locals:
+    #
+    #  class MySpecialPage < Leviticus::Page
+    #    locals :author => "Jack Danger",
+    #           :show_contact_form => true
+    #
+    def locals variables
+      variables.each do |key, value|
+        if respond_to?(key)
+          raise %Q{#{self.class} already has a "#{key}" method, pick a different name}
+        end
+        define_method(key) { value }
+      end
+    end
+
+    def prepare
+      raise "Please subclass the prepare method on #{self.class}. This is where you set up all page content before the view is processed."
     end
 
     def write
       File.open(outfile, 'w') {|f| f.write render }
-    end
-
-    def decrement_string string
-      length = string.length
-      (string.to_i - 1).to_s.rjust(length, '0')
-    end
-
-    def increment_string string
-      string.succ
     end
 
     def render
@@ -27,265 +46,15 @@ module Leviticus
     end
 
     def template
-      @template ||= Haml::Engine.new(File.read(ViewsDirectory + '/template.haml'), :filename => 'template')
+      template_file    = File.join Leviticus.views, "template.#{medium}.haml"
+      template_content = File.read template_file
+      Haml::Engine.new template_content, :filename => 'template'
     end
 
     def view
-      src = is_kindle? ? view_file.chomp('.haml') + '.kindle.haml' : view_file
-      Haml::Engine.new(File.read(ViewsDirectory + '/' + src), :filename => src)
-    end
-
-    alias is_kindle? is_kindle
-
-    def stylesheet_path
-      '../style.css'
-    end
-
-    def facebook_like
-      %Q{<div id="fb-root"></div>
-<script>
-window.fbAsyncInit = function() {
-FB.init({appId: '186246214732289', status: true, cookie: true,
-xfbml: true});
-};
-</script><fb:like href="http://silentvoicesbible.com/#{path_from(SilentVoices.start_page)}" show_faces="false" width="120" action="like" font="lucida grande" layout="button_count"></fb:like>}
-    end
-
-  end
-
-  class StartPage < Page
-    def initialize
-      super
-      SilentVoices.start_page = self
-    end
-
-    def name
-      'Silent Voices'
-    end
-    def prev_page; nil; end
-    def next_page; nil; end
-
-    def path_from(page)
-      page.is_a?(StartPage) ? 'index.html' :  "../index.html"
-    end
-
-    def stylesheet_path
-      'style.css'
-    end
-
-    def title
-      "Silent Voices | The Feminist Bible For a Billion Women"
-    end
-
-    def view_file
-      'start_page.haml'
-    end
-
-    def outfile
-      SilentVoices::Directory + '/index.html'
-    end
-
-    def blog_links
-      Dir.glob(Directory + '/_blog_src/_posts/*.textile')[0,5].map do |filename|
-
-        [ 'blog/' + filename.split('/').last.sub(/\d{4}-\d{2}-\d{2}-/, '').chomp('.textile') + '.html',
-          File.read(filename).scan(/title: (.*)/).flatten.first
-        ]
-
-      end
-    end
-
-    def write
-      puts ''
-      print "writing: #{name} => index.html"
-      super
-    end
-  end
-
-  class IndexPage < Page
-
-    def initialize
-      super
-      SilentVoices.index_page = self
-    end
-
-    def name
-      'Silent Voices Bible'
-    end
-    def title
-      "Silent Voices | Feminist Translations of Books of the Bible"
-    end
-
-    def prev_page; nil; end
-    def next_page; nil; end
-
-    def outfile
-      SilentVoices::Directory + '/voices/index.html'
-    end
-
-    def book_pages
-      @book_pages ||= SilentVoices.pages.select {|page| BookPage === page }.sort_by(&:number)
-    end
-
-    def books_grouped
-      [
-        ['Old Testament',
-          [
-            ['The Torah', book_pages[0, 5]],
-            ['Communal History', book_pages[5, 13]],
-            ['Poetry',          book_pages[18, 4]],
-            ['Major Prophets',  book_pages[22, 5]],
-            ['Minor Prophets',  book_pages[27, 12]]
-          ]
-        ],
-        ['New Testament',
-          [
-            ['The Gospels', book_pages[39, 5]],
-            ['The Letters', book_pages[44, 21]],
-            ['Apokalypsis', book_pages[65, 1]]
-          ]
-        ]
-      ]
-    end
-
-    def path_from(page)
-      page.is_a?(StartPage) ? 'voices/index.html' : 'index.html'
-    end
-
-    def outfile
-      SilentVoices::Directory + '/voices/index.html'
-    end
-
-    def view_file
-      'index.haml'
-    end
-
-    def stylesheet_path
-      '../style.css'
-    end
-
-    def write
-      puts ''
-      print "writing: #{name} => voices/index.html"
-      super
-    end
-  end
-
-  class BookPage < Page
-    attr_accessor :book
-    def initialize book
-      @book = book
-      super
-    end
-
-    def prev_page
-      @prev_page ||= SilentVoices.pages.detect {|page| page.is_a?(BookPage) && decrement_string(number).eql?(page.number) }
-    end
-
-    def next_page
-      @next_page ||= SilentVoices.pages.detect {|page| page.is_a?(BookPage) && increment_string(number).eql?(page.number) }
-    end
-
-    def chapter_pages
-      @chapter_pages ||= SilentVoices.pages.select {|page| ChapterPage === page && book == page.book }.sort_by(&:number)
-    end
-
-    def name
-      book[:name]
-    end
-
-    def number
-      book[:number]
-    end
-
-    def title
-      "The Feminist Bible | #{name}"
-    end
-
-    def filename
-      "#{number}-#{name.downcase.gsub(' ', '_')}.html"
-    end
-
-    def path_from(page)
-      page.is_a?(StartPage) ? "voices/#{filename}" : filename
-    end
-
-    def outfile
-      SilentVoices::Directory + '/voices/' + filename
-    end
-
-    def view_file
-      'book.haml'
-    end
-
-    def write
-      puts ''
-      print "writing: #{name} => "
-      super
-    end
-  end
-
-  class ChapterPage < Page
-    attr_accessor :book
-
-    def initialize chapter, book
-      @chapter = chapter
-      @book = book
-      super
-    end
-
-    def book_page
-      @book_page ||= SilentVoices.pages.detect {|page| book[:name] == page.name }
-    end
-
-    def prev_page
-      @prev_page ||=
-        SilentVoices.pages.detect {|page| name(decrement_string(number)) == page.name } ||
-        SilentVoices.pages.detect {|page| page.is_a?(BookPage) && decrement_string(book[:number]).eql?(page.number) }
-    end
-
-    def next_page
-      @next_page ||=
-        SilentVoices.pages.detect {|page| name(increment_string(number)) == page.name } ||
-        SilentVoices.pages.detect {|page| page.is_a?(BookPage) && increment_string(book[:number]).eql?(page.number) }
-    end
-
-    def name(num = number)
-      "#{book[:name]} #{num.to_i}"
-    end
-
-    def number
-      @chapter[:number]
-    end
-
-    def verses
-      @chapter[:verses]
-    end
-
-    def title
-      "The Feminist Bible | #{name}"
-    end
-
-    def filename
-      "#{book[:number]}-#{book[:name].downcase.gsub(' ', '_')}-#{number}.html"
-    end
-
-    def path_from(page)
-      page.is_a?(StartPage) ? "voices/#{filename}" : filename
-    end
-
-    def outfile
-      SilentVoices::Directory + '/voices/' + filename
-    end
-
-    def view_file
-      'chapter.haml'
-    end
-
-    def write
-      print '.'
-      STDOUT.flush
-      super
+      view_file    = File.join Leviticus.views, "#{Leviticus.underscore self.class.name}.#{medium}.haml"
+      view_content = File.read view_file
+      Haml::Engine.new view_content, :filename => src
     end
   end
 end
