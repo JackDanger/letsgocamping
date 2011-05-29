787ae6db2f395832275fb393ed71fe2fab172285
Jack Danger Canty <gitcommit@6brand.com>
2011-05-29 14:34:14 -0700

    initialize isn't needed

--- vendor/leviticus/lib/source.rb
+++ vendor/leviticus/lib/source.rb
@@ -4,14 +4,6 @@ module Leviticus
     attr_reader :content
     attr_accessor :owner
 
-    def initialize *args
-      raise <<-DOC
-        Please subclass the initialize method of your source.
-        It should store a `@content` instance variable with the
-        full contents of your source material
-      DOC
-    end
-
     def process *media
       run_compiler!
       media.each do |medium|
@@ -25,7 +17,7 @@ module Leviticus
         unless respond_to? :compile
           raise <<-DOC
             Please define a `compile` method to your #{self.class} class.
-            This should read the @content string and instantiate each page object,
+            This should read all source data and instantiate each page object,
             assigning to each page the values needed to render a complete html document.
           DOC
         end
