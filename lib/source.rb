6956f4eee749cd6e6541b0dde92b03dbf1a9b79b
Jack Danger Canty <gitcommit@6brand.com>
2011-05-29 13:49:17 -0700

    using intermediate sqlite database

--- lib/source.rb
+++ lib/source.rb
@@ -3,7 +3,9 @@ require 'activesupport'
 require 'nokogiri'
 module Recreation
   class Source < Leviticus::Source
+
     DataDotGov = File.join File.dirname(__FILE__), '..', 'vendor', 'us_government_recreation_sites_and_facilities.xml'
+    DBFile     = File.join File.dirname(__FILE__), '..', 'vendor', 'db.sqlite'
 
     def initialize
       @content = File.read DataDotGov
@@ -15,35 +17,36 @@ module Recreation
 
     protected
       def database
-        @database ||= SQLite3::Database.new "test.db"
+        @database ||= SQLite3::Database.new DBFile
       end
 
       def parse_xml!
         @doc = Nokogiri::XML.parse @content
-        xml_node_types.each do |node_type|
-          @doc.search node_type do |variable|
-            
+        xml_record_types.each do |record_type|
+          @doc.search "arc:#{record_type}" do |variable|
+            p variable
           end
+          break
         end
       end
 
-      def xml_node_types
+      def xml_record_types
         %w{
-          arc:Event
-          arc:Facility
-          arc:FacilityActivity
-          arc:FacilityAddress
-          arc:FacilityEvent
-          arc:OrgFacilityRole
-          arc:OrgRecAreaRole
-          arc:Organization
-          arc:RecArea
-          arc:RecAreaActivity
-          arc:RecAreaAddress
-          arc:RecAreaAdvisory
-          arc:RecAreaEvent
-          arc:RecAreaFacility
-          arc:RecreationalActivity
+          Event
+          Facility
+          FacilityActivity
+          FacilityAddress
+          FacilityEvent
+          OrgFacilityRole
+          OrgRecAreaRole
+          Organization
+          RecArea
+          RecAreaActivity
+          RecAreaAddress
+          RecAreaAdvisory
+          RecAreaEvent
+          RecAreaFacility
+          RecreationalActivity
         }
       end
   end
